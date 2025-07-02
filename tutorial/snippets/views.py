8fc7b688db7685dcd040b080c78f67663c26e52d
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-07-02 15:32:35 +0200

    Refactor `Snippet` views to use generic class-based views.
    Replaced custom `APIView` implementations with generic views using `mixins` for improved readability, maintainability, and consistency. Added serializers for URL parameter validation and enhanced type hinting for cleaner and safer code.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,57 +1,111 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.http import Http404
+# from rest_framework.decorators import api_view
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from django.http import Http404
 from tutorial.snippets.models import Snippet
 from tutorial.snippets.serializers import SnippetSerializer
-# from rest_framework.decorators import api_view
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework import serializers
+from dataclasses import dataclass
 
-class SnippetList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+@dataclass
+class SnippetParams:
+    pk: int
 
-    # Create Snippet
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UrlParamsSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(min_value=1)
 
-class SnippetDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
+    def validate_pk(self, value):
+        # E.G business rull
+        if value == 999:
+            raise serializers.ValidationError("Valeur réservée")
+        return value
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-## FUNCTION BASED VIEW ##
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs: SnippetParams):
+
+        # Validate with serializer
+        params_serializer = UrlParamsSerializer(data=kwargs)
+        params_serializer.is_valid(raise_exception=True)
+
+        # Typing for autocompletion
+        validated_params = SnippetParams(**params_serializer.validated_data)
+        print("validated_params ", validated_params)
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+# ---- CLASS BASED VIEW ----
+# class SnippetList(APIView):
+#     """
+#     List all snippets, or create a new snippet.
+#     """
+#     def get(self, request, format=None):
+#         snippets = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets, many=True)
+#         return Response(serializer.data)
+#
+#     # Create Snippet
+#     def post(self, request, format=None):
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# class SnippetDetail(APIView):
+#     """
+#     Retrieve, update or delete a snippet instance.
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return Snippet.objects.get(pk=pk)
+#         except Snippet.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         snippet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# ---- FUNCTION BASED VIEW ----
 # @api_view(['GET', 'POST'])
 # def snippet_list(request, format=None):
 #     """
